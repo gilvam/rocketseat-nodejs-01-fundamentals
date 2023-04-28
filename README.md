ce02a0fb00b627d5e15a2692d01ad69e4e714352
Gilvam Mourão <gilvamm@gmail.com>
2023-04-28 16:56:33 -0300

    feat: list user with filter

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
 # rocketseat-nodejs-01-fundamentals
 
-### init projet:
+### init projet
 `` npm run dev ``
 
 
 ## REST
 
-### POST /users
+### POST user
 #### request: http://localhost:3333/users
 #### body: `` { "name": "...", "email": "..." } ``
 #### response: `` 201 ``
 
-### GET /users
+### GET users
 #### request: http://localhost:3333/users
 #### response: `` [{ "name": "...", "email": "..." }] ``
 
+### GET users search
+#### request: http://localhost:3333/users?search=NAME_OR_EMAIL
+#### response: `` [{ "name": "...", "email": "..." }] ``
+
+### PUT user
+#### request: http://localhost:3333/users/:ID
+#### body: `` { "name": "...", "email": "..." } ``
+#### response: `` 204 ``
+
+### DELETE user
+#### request: http://localhost:3333/users/:ID
+#### response: `` 204 ``
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
 	const {method, url} = req
@@ -10,10 +11,13 @@ const server = http.createServer(async (req, res) => {
 	const route = routes.find(route => route.method === method && route.path.test(url))
 
 	if (route) {
-		const routeParams = req.url.match(route.path);
-		req.params = {...routeParams.groups}
+		const routeParams = req.url.match(route.path)
+		const {query, ...params} = routeParams.groups
 
-		return route.handler(req, res);
+		req.params = params
+		req.query = query ? extractQueryParams(query) : {}
+
+		return route.handler(req, res)
 	}
 
 	return res.writeHead(404).end()
